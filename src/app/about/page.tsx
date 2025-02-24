26b683b2e237e91100ed2d9ee6b72946b1e0054e
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-02-24 04:49:08 +0300

    about page page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+
+export default function About() {
+  return (
+    <div>
+      <Header />
+      <h1>About Us</h1>
+      <ul>
+        <li>Mission & Vision</li>
+        <li>Press</li>
+        <li>Careers</li>
+        <li>Contact</li>
+      </ul>
+      <Footer />
+    </div>
+  );
+}
